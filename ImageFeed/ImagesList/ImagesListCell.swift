b8b0562d749419cb434300abfc4ba779af9b3f7a
Mnkvch <mankevich.andrey@gmail.com>
2023-05-17 00:31:28 +0300

    исправил критические замечания к ДЗ спринт 8

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -5,14 +5,13 @@
 //  Created by Андрей Манкевич on 10.05.2023.
 //
 
-import Foundation
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
 
     static let reuseIdentifier = "ImagesListCell"
-    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
     
-    @IBOutlet weak var likeButton: UIButton!
-    @IBOutlet weak var dateLabel: UILabel!
 }
