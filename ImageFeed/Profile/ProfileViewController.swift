245f3b41e60109c9d97cf71cc4f48a001b1148eb
Mnkvch <mankevich.andrey@gmail.com>
2023-05-19 00:54:49 +0300

    Сверстал экран профиля пользователя
    Настроил UITabBarController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Манкевич on 18.05.2023.
+//
+
+import UIKit
+
+final class ProfileViewConroller: UIViewController {
+    
+    
+    @IBOutlet weak var Avatar: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var loginLadel: UILabel!
+    
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBAction func TapLogoutButton(_ sender: Any) {
+    }
+    
+}
