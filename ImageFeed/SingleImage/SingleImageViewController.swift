036d563e9de766607e26246fbcf4d2db8ff79483
Mnkvch <mankevich.andrey@gmail.com>
2023-05-21 23:50:22 +0300

    сверстаy экран для показа одного изображения

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Манкевич on 21.05.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var singleImage: UIImageView!
+    
+    
+}
